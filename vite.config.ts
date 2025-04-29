9c8fc05afc3c5ca52d14499a195cd54082ac93f3
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2025-04-29 22:24:44 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "kitty-kibble-creations-main",
+  base: '/kitty-kibble-creations-main/',
   server: {
     host: "::",
     port: 8080,
